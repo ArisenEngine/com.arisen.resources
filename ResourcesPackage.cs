09c0c4a8becad88e3cdf110df25481c0a795075f
eatdreamcat <meow.std@outlook.com>
2026-07-06 14:57:34 +0800

    enhance ResourcesPackage to log asset indexing and release loaded assets

--- ResourcesPackage.cs
+++ ResourcesPackage.cs
@@ -1,6 +1,8 @@
 using ArisenKernel.Packages;
 using ArisenKernel.Services;
 using ArisenKernel.Diagnostics;
+using ArisenKernel.Lifecycle;
+using ArisenEngine.Core.Assets;
 
 namespace ArisenEngine.Resources;
 
@@ -8,10 +10,25 @@ public class ResourcesPackage : IPackageEntry
 {
     public void OnLoad(IServiceRegistry registry)
     {
-        KernelLog.Info("[ResourcesPackage] Loaded: Arisen Asset Resources");
+        var config = EngineKernel.Instance.Config;
+        var workspaceRoot = config?.ProjectRoot ?? Directory.GetCurrentDirectory();
+        var packages = (config?.PackageUrls ?? new List<string>())
+            .Where(Directory.Exists)
+            .Select(path => (PackageId: Path.GetFileName(path), PackageRoot: Path.GetFullPath(path)))
+            .ToArray();
+
+        var database = AssetDatabase.Instance;
+        database.InitializeWorkspace(workspaceRoot, packages);
+        registry.RegisterService<IAssetDatabase>(database);
+
+        KernelLog.InfoFormat(
+            "[ResourcesPackage] Loaded: indexed {0} asset(s), cooked root '{1}'.",
+            database.Assets.Count,
+            database.CookedRoot);
     }
 
     public void OnUnload(IServiceRegistry registry)
     {
+        AssetDatabase.Instance.ReleaseAllLoadedCookedAssets();
     }
 }
